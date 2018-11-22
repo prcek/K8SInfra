23618d4383bc60c5b7f6b054e354bd234caea3a9
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-11-22 17:14:22 +0100

    up

--- certbot/entry.sh
+++ certbot/entry.sh
@@ -7,12 +7,19 @@
 #fi
 
 
-certbot certonly --standalone  --test-cert -m tomas.hluchan@gmail.com -d www.prcek.io -d do.tsstarlet.net --preferred-challenges http -n --agree-tos
+#certbot certonly --standalone  --test-cert -m tomas.hluchan@gmail.com -d www.prcek.io -d do.tsstarlet.net --preferred-challenges http -n --agree-tos
 
 
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 CACERT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 curl --cacert $CACERT  -H "Authorization: Bearer $TOKEN"  https://kubernetes/api/v1/services
+
+kubectl config set-cluster cfc --server=https://kubernetes.default --certificate-authority=$CACERT
+kubectl config set-context cfc --cluster=cfc
+kubectl config set-credentials user --token=$TOKEN
+kubectl config set-context cfc --user=user
+kubectl config use-context cfc
+
 
 #certbot certonly --standalone  --test-cert  -n --agree-to -m tomas.hluchan@gmail.com -d do.tsstarlet.net --preferred-challenges http
