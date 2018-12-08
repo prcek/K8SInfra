a2ebee32dea88c9022f4841a6d6086d58a579a17
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-12-08 22:15:58 +0100

    note tests

--- fullstack/server/src/datasources/__tests__/demo.js
+++ fullstack/server/src/datasources/__tests__/demo.js
@@ -2,16 +2,24 @@
 
 
 const { createMockMongoStore } = require('../../utils');
+const NoteAPI = require('../note');
+
+
 
 let store;
 let UserModel;
 let NoteModel;
+let noteAPI;
+
+
 
 beforeAll(async ()=>{
     console.log("beforeAll")
     store = await createMockMongoStore();
     UserModel = store.UserModel;
-    NoteMode = store.NoteModel;
+    NoteModel = store.NoteModel;
+    noteAPI = new NoteAPI({ store });
+    noteAPI.initialize({ context: { user: { id: 1, email: 'a@a.a' } } });
 });
 
 afterAll( async () => {
@@ -22,11 +30,11 @@ afterAll( async () => {
   
 
 
-describe('[DemoAPI.Mongoose 1]',  () => {
+describe('[Mongoose UserModel]',  () => {
     test('UserModel.count is 0', async () => {
         expect(await UserModel.countDocuments()).toEqual(0);
     });
-    it('new UserModel', async () => {
+    test('new UserModel', async () => {
         var user = await UserModel.create({login:"foo",password:"bar"});
         expect(user).toMatchObject({login:"foo",password:"bar"});
         //expect(user).not.toHaveProperty('_id');
@@ -34,18 +42,29 @@ describe('[DemoAPI.Mongoose 1]',  () => {
         expect(await UserModel.countDocuments()).toEqual(1);
     });
   
-    it('UserModel.count is 1', async () => {
+    test('UserModel.count is 1', async () => {
         await expect(UserModel.countDocuments()).resolves.toEqual(1);
     });
 
-    it('new UserModel - duplicate login', async () => {
+    test('new UserModel - duplicate login', async () => {
         await expect(UserModel.create({login:"foo",password:"bar"}) ).rejects.toThrow();
         expect( await UserModel.countDocuments()).toEqual(1);
     });
 
-    it('UserModel.count is still 1', async () => {
+    test('UserModel.count is still 1', async () => {
         expect(await UserModel.countDocuments()).toEqual(1);
     });
 
 })
 
+describe('[NoteAPI]',  () => {
+    test('NoteAPI.notes is []', async () => {
+        expect(await noteAPI.getAllNotes()).toEqual([]);
+    });
+    
+    test('NoteAPI new note', async () => {
+        expect(await noteAPI.createNote({note:"test"})).toMatchObject({note:"test"});
+        expect(await noteAPI.getAllNotes()).toContainEqual(expect.objectContaining({ note: "test", id: expect.anything() }));
+    });
+    
+})
